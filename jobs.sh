d46863d9043ddd958c0e72ba4d3333751b997a4d
RongqinChen <yc07917@umac.mo>
2026-04-18 17:58:27 +0800

    update

--- jobs.sh
+++ jobs.sh
@@ -1,8 +1,10 @@
 source ~/miniforge3/bin/activate gnn270
 
-for poly_dim in 8
+export CUDA_VISIBLE_DEVICES=1
+
+for poly_dim in 4
 do
 
-python run_peptides_struct.py --cfg configs/rsed_ppgn/pep_str.rse_ppgn.poly.yaml --poly_dim $poly_dim 
+python run_peptides_struct.py --cfg configs/rsed_ppgn/pep_str.rsed2_ppgn.poly.yaml --poly_dim $poly_dim 
 
 done
