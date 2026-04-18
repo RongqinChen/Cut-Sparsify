0f4778f5f44f0645a45c52b4827f6e930e6dbccf
RongqinChen <yc07917@umac.mo>
2026-04-18 16:56:29 +0800

    update

--- jobs.sh
+++ jobs.sh
@@ -1,6 +1,6 @@
 source ~/miniforge3/bin/activate gnn270
 
-for poly_dim in 8
+for poly_dim in 4
 do
 
 python run_peptides_struct.py --cfg configs/rsed_ppgn/pep_str.rse_ppgn.poly.yaml --poly_dim $poly_dim 
