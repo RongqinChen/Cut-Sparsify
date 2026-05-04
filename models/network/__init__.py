56df3c518b8ad759ded28e096de16d9bf09707b7
RongqinChen <yc07917@umac.mo>
2026-05-04 19:11:41 +0000

    update

--- models/network/__init__.py
+++ models/network/__init__.py
@@ -1,7 +1,10 @@
 from .ppgn import PPGN
 from .sppgn import SPPGN
+from .cat_mlp_2fwl import CatMLP_2FWLGNN
 
 network_dict = {
     "ppgn": PPGN,
     "sppgn": SPPGN,
+    "mlp_prod_2fwl": SPPGN,
+    "cat_mlp_2fwl": CatMLP_2FWLGNN,
 }
